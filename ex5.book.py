b85ce7f9b1edda7ae6a83ed37052712fc8d73f6c
chandra dhiman <chandradhiman13@gmail.com>
2024-05-03 17:30:44 +0200

    add new files

---
+++ ex5.book.py
@@ -0,0 +1,9 @@
+from sys import argv
+script, first, second, third = argv
+print("The script called:", script)
+print("Your first variable is:", first)
+print("Your second variable is:", second)
+print("Your third variable is:", third)
+
+#argv1 = input ("2")
+#print("hello how are you")
--- voting.py
+++ voting.py
@@ -1,9 +1,9 @@
 print("Please enter valid argument in between 10-18")
 a = int(input("Enter Your Age: "))
 def voting (a):
-    if a in (10, 11, 12, 13, 14, 15, 16, 17):
+    if a in range (10, 17):
         result ="Sorry! You're not eligble for voting."
-    elif a in (18, 19, 20):  
+    elif a in range (18,40):  
         result ="Congrts, You're eligble for voting." 
     else:
         result ='Please enter valid argument 10-20.' 
